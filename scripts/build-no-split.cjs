5e638517825a2b915517103a0077c0b89cd51504
Mark Sutton <msutton@urban.org>
2020-01-13 10:47:44 -0500

    use external react libs and build with no css and js split.

---
+++ scripts/build-no-split.cjs
@@ -0,0 +1,24 @@
+
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/build.js');
+const config = defaults.__get__('config');
+
+// Use external version of React
+config.externals = {
+  "react": "React",
+  "react-dom": "ReactDOM"
+};
+
+// Consolidate chunk files instead
+config.optimization.splitChunks = {
+  cacheGroups: {
+    default: false,
+  },
+};
+// Move runtime into bundle instead of separate file
+config.optimization.runtimeChunk = false;
+
+// JS
+config.output.filename = 'static/js/[name].js';
+// CSS. "5" is MiniCssPlugin
+config.plugins[5].options.filename = 'static/css/[name].css';
